26cb183f49b8b74b12c5ab179e0d3861d38f2237
Waqas <penkardbyzainab@gmail.com>
2024-05-27 02:06:58 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>
+      Hello Future!
+     </h1>
+    <p>
+      Zainab Waqas
+    </p>
+    </>
+  )
+}
+
+export default App
